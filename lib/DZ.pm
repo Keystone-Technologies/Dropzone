15763a5faab43d9ca26877b8d593c41d034974fd
Stefan Adams <Stefan.Adams@keystone-it.com>
2014-04-30 15:29:24 +0000

    fix upload

--- lib/DZ.pm
+++ lib/DZ.pm
@@ -16,7 +16,7 @@ has app => sub { shift->{app} };
 has log => sub { Mojo::Log->new };
 has _uuid => sub {
   my $self = shift;
-  local $_ = $self->app->param('session');
+  local $_ = $self->app->param('session') || $self->app->param('uuid');
   if ( /^[0-9]+$/ ) {
     return $self->map($_);
   } elsif ( /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/) {
