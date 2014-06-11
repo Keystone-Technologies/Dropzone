bac9daba4f5f5bbff1d88e660bb3b91bc1401b9f
Caleb Albers <calebalbers@Calebs-MacBook-Air.local>
2014-06-11 01:08:27 -0500

    Fixed legal lightbox issues

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,4 +1,4 @@
-﻿/****************************************
+/****************************************
 	Barebones Lightbox Template
 	by Kyle Schaeffer
 	http://www.kyleschaeffer.com
@@ -10,7 +10,7 @@ function lightbox(insertContent, ajaxContentUrl){
 
 	// jQuery wrapper (optional, for compatibility only)
 	(function($) {
-	
+
 		// add lightbox/shadow <div/>'s if not previously added
 		if($('#lightbox').size() == 0){
 			var theLightbox = $('<div id="lightbox"/>');
@@ -21,20 +21,20 @@ function lightbox(insertContent, ajaxContentUrl){
 			$('body').append(theShadow);
 			$('body').append(theLightbox);
 		}
-		
+
 		// remove any previously added content
 		$('#lightbox').empty();
-		
+
 		// insert HTML content
 		if(insertContent != null){
 			$('#lightbox').append(insertContent);
 		}
-		
+
 		// insert AJAX content
 		if(ajaxContentUrl != null){
 			// temporarily add a "Loading..." message in the lightbox
 			$('#lightbox').append('<p class="loading">Loading...</p>');
-			
+
 			// request AJAX content
 			$.ajax({
 				type: 'GET',
@@ -49,31 +49,31 @@ function lightbox(insertContent, ajaxContentUrl){
 				}
 			});
 		}
-		
+
 		// move the lightbox to the current window top + 100px
 		$('#lightbox').css('top', $(window).scrollTop() + 100 + 'px');
-		
+
 		// display the lightbox
 		$('#lightbox').show();
 		$('#lightbox-shadow').show();
-	
+
 	})(jQuery); // end jQuery wrapper
-	
+
 }
 
 // close the lightbox
 function closeLightbox(){
-	
+
 	// jQuery wrapper (optional, for compatibility only)
 	(function($) {
-		
+
 		// hide lightbox/shadow <div/>'s
 		$('#lightbox').hide();
 		$('#lightbox-shadow').hide();
-		
+
 		// remove contents of lightbox in case a video or other content is actively playing
 		$('#lightbox').empty();
-	
+
 	})(jQuery); // end jQuery wrapper
-	
+
 }
